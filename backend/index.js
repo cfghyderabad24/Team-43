1138ca961b7325581263feb2ba553881d3103e89
chandrakanth93986 <chandu93986@gmail.com>
2024-06-22 19:03:45 +0530

    Added backend of sending mail

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,28 @@ app.use(express.json())
 app.use("/api/user", userRoute)
 app.use("/api/auth", loginRouter)
 
+
+app.post("/api/sendemail", async (req, res) => {
+    const { email } = req.body;
+  
+    try {
+      const send_to = email;
+      const sent_from = process.env.EMAIL_USER;
+      const reply_to = email;
+      const subject = "Thank You Message From NodeCourse";
+      const message = `
+          <h3>Hello Zino</h3>
+          <p>Thank for your YouTube Tutorials</p>
+          <p>Regards...</p>
+      `;
+  
+      await sendEmail(subject, message, send_to, sent_from, reply_to);
+      res.status(200).json({ success: true, message: "Email Sent" });
+    } catch (error) {
+      res.status(500).json(error.message);
+    }
+  });
+
 //
 
 // app.use((req, res, next) => {
