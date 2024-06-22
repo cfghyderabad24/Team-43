7e6b12124bd64c84bbf6e708253abeb2f48e94df
VedanshiAwasthi <awasthivedanshi5@gmail.com>
2024-06-23 00:31:57 +0530

    update

--- frontend/src/components/Home1.jsx
+++ frontend/src/components/Home1.jsx
@@ -102,7 +102,7 @@ const ProductCard = ({ name, id, price, handler, imgSrc }) => (
   <div className="productCard">
     <img src={imgSrc} alt={name} />
     <p>{name}</p>
-    <h4>${price}</h4>
+    <h4>${price}</h4> 
     <button onClick={() => handler({ name, price, id, qty: 1, imgSrc })}>
       Add to Cart
     </button>
